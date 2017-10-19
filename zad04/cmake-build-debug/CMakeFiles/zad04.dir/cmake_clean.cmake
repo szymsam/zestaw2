8d8ba106828f604dc6f39bb3171a4a7951ba07a3
Szymon Samólnik <szymon.samolnik@gmail.com>
2017-10-19 19:51:01 +0200

    zadanie4

---
+++ zad04/cmake-build-debug/CMakeFiles/zad04.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zad04.dir/main.c.o"
+  "zad04.pdb"
+  "zad04"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zad04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ zad04/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    while (i<=10000)
+    {
+        i=i+2;
+        printf("%d ",i);
+    }
+    return 0;
+}
