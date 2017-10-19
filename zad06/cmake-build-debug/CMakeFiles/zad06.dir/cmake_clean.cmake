2300b225b7648d0e5a84087b03b14f255dbaa96e
Szymon Samólnik <szymon.samolnik@gmail.com>
2017-10-19 20:03:57 +0200

    zadanie6

---
+++ zad06/cmake-build-debug/CMakeFiles/zad06.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zad06.dir/main.c.o"
+  "zad06.pdb"
+  "zad06"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zad06.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
